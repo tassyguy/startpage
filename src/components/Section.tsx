9a593f5e480583cf7ba2fd37b1456b081600df00
Simon Phillips <simonxphillips@gmail.com>
2023-04-01 19:10:55 +0000

    Initial commit

---
+++ src/components/Section.tsx
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export default class Section extends Component {
+  // static propTypes = {second: third}
+
+  render() {
+    return (
+      <div>Section</div>
+    )
+  }
+}
