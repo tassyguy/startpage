aec54899068214a8190780f22a79cf77455aec1d
Simon Phillips <simonxphillips@gmail.com>
2023-10-20 01:08:55 -0400

    Updated Links

--- components/bookmarks/bookmark/bookmark.tsx
+++ components/bookmarks/bookmark/bookmark.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 interface BookmarkProps {
@@ -9,13 +10,13 @@ interface BookmarkProps {
 
 const Bookmark: React.FC<BookmarkProps> = ({ siteName, icon, url, description }) => (
   <div className="bookmark">
-    <a href={url} className="bookmark-link">
+    <Link href={url} className="bookmark-link">
       <img src={icon} alt={siteName} className="bookmark-icon" />
       <div className="bookmark-text">
         <h3 className="site-name">{siteName}</h3>
         <p className="description">{description}</p>
       </div>
-    </a>
+    </Link>
   </div>
 );
 
